61c5565a506b191a9fd3c73dbef2f3e13ae47bb5
iliarafa <83038866+iliarafa@users.noreply.github.com>
2021-08-17 12:44:51 +0300

    pre challenge setup

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ mongo = PyMongo(app)
 
 @app.route("/")
 def index():
-    mars = mongo.db.mars.find_one()
-    return render_template('index.html', mars = mars)
+   mars = mongo.db.mars.find_one()
+   return render_template("index.html", mars=mars)
 
 @app.route("/scrape")
 def scrape():
--- scraping.py
+++ scraping.py
@@ -1,38 +1,37 @@
 # Import Splinter, BeautifulSoup, and Pandas
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
-from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
 import datetime as dt
+from webdriver_manager.chrome import ChromeDriverManager
+
 
 def scrape_all():
     # Initiate headless driver for deployment
     executable_path = {'executable_path': ChromeDriverManager().install()}
     browser = Browser('chrome', **executable_path, headless=True)
-    
+
     news_title, news_paragraph = mars_news(browser)
 
-    
-    # Run all scraping functions and store results in dictionary
+    # Run all scraping functions and store results in a dictionary
     data = {
-      "news_title": news_title,
-      "news_paragraph": news_paragraph,
-      "featured_image": featured_image(browser),
-      "facts": mars_facts(),
-      "last_modified": dt.datetime.now()
-}
+        "news_title": news_title,
+        "news_paragraph": news_paragraph,
+        "featured_image": featured_image(browser),
+        "facts": mars_facts(),
+        "last_modified": dt.datetime.now()
+    }
 
     # Stop webdriver and return data
     browser.quit()
     return data
 
 
-# Visit Mars news site
 def mars_news(browser):
 
     # Scrape Mars News
     # Visit the mars nasa news site
-    url = 'https://data-class-mars.s3.amazonaws.com/Mars/index.html'
+    url = 'https://redplanetscience.com'
     browser.visit(url)
 
     # Optional delay for loading the page
@@ -55,9 +54,7 @@ def mars_news(browser):
 
     return news_title, news_p
 
-# ## JPL Space Images Featured Image
 
-# Visit URL
 def featured_image(browser):
     # Visit URL
     url = 'https://spaceimages-mars.com'
@@ -84,8 +81,6 @@ def featured_image(browser):
 
     return img_url
 
-# ## Mars Facts
-
 def mars_facts():
     # Add try/except for error handling
     try:
@@ -103,5 +98,6 @@ def mars_facts():
     return df.to_html(classes="table table-striped")
 
 if __name__ == "__main__":
+
     # If running as script, print scraped data
     print(scrape_all())
